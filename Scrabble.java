21864095ccc50456a4c45d757ce5a84aa78dd166
tdavis298 <tuckkeeper@gmail.com>
2017-09-02 19:41:04 -0400

    Finished

--- Scrabble.java
+++ Scrabble.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Scrabble {
   public static void main(String[] args) throws Exception {
     int y = 0;
+    int t = 0;
     Scanner s = new Scanner(System.in);
     Scanner su = new Scanner(System.in);
     //Creates new scanners to take user input
@@ -26,20 +27,34 @@ public class Scrabble {
     String[] word = su.nextLine().split("");
     //Creates an array to take the word
 
+    int u = word.length;
+
     //Just to test
-    System.out.println(Arrays.toString(tiles));
-    System.out.println(Arrays.toString(word));
+    //System.out.println(Arrays.toString(tiles));
+    //System.out.println(Arrays.toString(word));
 
     for (int x = 0; x < word.length; x++){
       for(int z = 0; z < tiles.length; z++){
         String letter = word[x];
         String letterone = tiles[z];
         int result = letter.compareTo(letterone);
-        System.out.println(result);
+        if(result == 0){
+          t = t + 1;
+        }
         //prints the comparison of each letter in the word to each letter
         //in the tiles, with the difference between the placements in the
         //alphabet being the number it returns
       }
     }
+    //Used for testing
+    //System.out.println("split");
+    //System.out.println(t);
+    if(t >= u){
+      System.out.println("Can create word.");
+    } else {System.out.println("Cannot create word.");
+    }
+    //every time the compareTo class returns a value of 0, the int t has
+    //one added to it. If the result of t is greater than or equal to
+    //the length of the word, there must be enough letters to create it.
   }
 }
